f41368dea06d128d0b91bbdce32da8fc2f353a21
manish <manishdayma65@gmail.com>
2019-11-09 00:52:46 +0530

    2.0.3 login and register screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:owomark/dashboard_screen.dart';
-import 'package:owomark/welcome.dart';
-import 'home_screen.dart';
+import 'package:owomark/login_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,23 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Chat UI',
+      title: 'Owomark',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
           primaryColor: Colors.blueAccent,
-        accentColor: Color(0xFFFE9EB),
-          hintColor: Colors.blueAccent
-      ),
-      home: WelcomePage(),
+          accentColor: Color(0xFFFE9EB),
+          hintColor: Colors.blueAccent),
+      home: LoginScreen(),
     );
   }
 }
